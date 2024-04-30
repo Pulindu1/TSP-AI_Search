b74cb060e29e8b9433ba3df73b0813729f8032df
Pulindu1 <cfcf66@durham.ac.uk>
2024-04-30 05:04:43 +0100

    new best tours added

--- cfcf66/AlgBenhanced.py
+++ cfcf66/AlgBenhanced.py
@@ -157,7 +157,7 @@ def read_in_algorithm_codes_and_tariffs(alg_codes_file):
 ############
 ############ END OF SECTOR 0 (IGNORE THIS COMMENT)
 
-input_file = "AISearchfile058.txt"
+input_file = "AISearchfile535.txt"
 
 ############ START OF SECTOR 1 (IGNORE THIS COMMENT)
 ############
@@ -554,7 +554,7 @@ def Ant_Colony_Optimisation(dist_matrix, max_it, num_ants, pheramone_lvl, alpha,
 
 ### HYPER-PARAMETERS ###
 TIME_LIMIT = 300
-max_it = 100000
+max_it = 1000
 num_ants = 20
 pheramone_lvl = 0.5
 pheramone_decay_rate = None
